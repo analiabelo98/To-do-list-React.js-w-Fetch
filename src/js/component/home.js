d94c4770e526ab9e39d6b828fa4802c0e8af8472
analiabelo98 <analiabelo.42@gmail.com>
2021-05-07 14:00:13 +0000

    To do list w

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,73 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState } from "react";
 
 //create your first component
 export function Home() {
+	const [input, setInput] = useState("");
+	const [ToDo, setToDo] = useState([]);
+	const [placeholder, setPlaceholder] = useState("What needs to be done?");
+	const [style, setStyle] = useState("d-block");
+
+	let GetInput = e => {
+		e.preventDefault();
+		if (input != "") {
+			let ToDoCopy = [...ToDo];
+			ToDoCopy.push(input);
+			setToDo(ToDoCopy);
+			setInput("");
+			setPlaceholder("");
+		}
+	};
+
+	let removeItem = element => {
+		let ToDoCopy = [...ToDo];
+		setToDo(ToDoCopy.filter(input => input !== element));
+		if (ToDo.length == 1) {
+			setPlaceholder("No tasks, add a task");
+		} else {
+			setPlaceholder("");
+		}
+	};
+
+	// let changeStyle = () => {
+	// 	setStyle("d-block");
+	// };
+
 	return (
 		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<p className="display-1">to do&apos;s</p>
+			<div className="container w-50">
+				<form onSubmit={GetInput}>
+					<div className=" row">
+						<input
+							type="text"
+							className="list-group-item w-100 col-11"
+							placeholder={placeholder}
+							onChange={() => setInput(event.target.value)}
+							value={input}
+						/>
+
+						<button type="submit" className="btn  col">
+							<i className="far fa-plus-square"></i>
+						</button>
+					</div>
+				</form>
+				<ul className="list-group row">
+					{ToDo.map((element, id) => (
+						<li
+							key={id}
+							className="list-group-item text-left text-secondary h-150 d-flex justify-content-between">
+							{element}
+							<i
+								type="button"
+								className={"fas fa-trash text-right " + style}
+								onClick={() => removeItem(element)}></i>
+						</li>
+					))}
+					<div className="list-group-item text-muted">
+						{ToDo.length} item left
+					</div>
+				</ul>
+			</div>
 		</div>
 	);
 }
